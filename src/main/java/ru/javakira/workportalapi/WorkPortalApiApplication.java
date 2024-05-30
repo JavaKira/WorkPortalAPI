11d292d29d0a32a8ecd8d6861145c46a480f33b1
JavaKira <Javakira@yandex.ru>
2024-05-30 11:08:11 +0300

    first commit

---
+++ src/main/java/ru/javakira/workportalapi/WorkPortalApiApplication.java
@@ -0,0 +1,13 @@
+package ru.javakira.workportalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkPortalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkPortalApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/javakira/workportalapi/WorkPortalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.javakira.workportalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkPortalApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
